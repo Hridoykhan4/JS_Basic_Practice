27a3d276ba7a125952a4e67e4a27ae848f19b006
Md. Toyob Uddin Hridoy <hridoykhan148385@gmail.com>
2026-03-12 16:20:17 +0600

    practiced string methods (padStart, slice) & loop

---
+++ beforeNextLevel.js
@@ -0,0 +1,14 @@
+// const id = '7'
+// console.log(id.padStart(2, 0));
+
+// console.log((1).toString());
+// console.log(String(3));
+
+
+// const order = 1..toString().padStart(4, '0').substring(3)
+// console.log((Number(order) + 1).toString().padStart(3, 0));
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i+ ' Kamal');
+// }
+
