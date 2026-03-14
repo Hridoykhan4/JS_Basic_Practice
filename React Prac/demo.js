7da5370259a10de04ab091b811a3def183d30c4e
Md. Toyob Uddin Hridoy <hridoykhan148385@gmail.com>
2026-03-14 17:09:09 +0600

    integrated Babel to know about react compilation method

--- React Prac/demo.js
+++ React Prac/demo.js
@@ -1,4 +1,20 @@
-// const addEl = document.createElement("p");
-// addEl.innerText = "Added from React";
-// document.getElementById("root").appendChild(addEl);
-// console.log(addEl)
+// console.log(React);
+// console.log(ReactDOM);
+
+// const addEl = document.createElement('p')
+// addEl.textContent = `I am added by React`
+// document.getElementById('root').appendChild(addEl)
+// console.log(addEl);
+
+// For react
+
+// const reactEl = (
+//   <div>
+//     <p>I am added by React</p>
+//   </div>
+// );
+
+const myEl = React.createElement("p", null, "I am again added by React");
+ ReactDOM.createRoot(document.getElementById("root")).render(myEl);
+console.log(myEl);
+// ReactDOM.createRoot(document.getElementById('root')).render(reactEl)
