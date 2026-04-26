8fcad4a2dca061fb3ae8d15d5c17fb8deff50dc7
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-04-26 20:57:03 +0600

    TODO updated

--- PRE BOOTCAMP/webAPI.js
+++ PRE BOOTCAMP/webAPI.js
@@ -1,21 +1,17 @@
-// wait ==>  talk to server ==> file read / write   
-
+// wait ==>  talk to server ==> file read / write
 
 // fetch("https://jsonplaceholder.typicode.com/todos")
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
-const displayTodos = (todos) => {
-  /* {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-} */
-  const todoContainer = document.getElementById("todo-container");
-  todoContainer.innerHTML = ``
+let allTodo = [];
+const todoContainer = document.getElementById("todo-container");
+const totalTodo = document.getElementById("total-todo");
+const displayTodos = (mainTodos, optional = []) => {
+  const todos = [...mainTodos, ...optional];
+  // todoContainer.innerHTML = ``
+  totalTodo.textContent = todos?.length ?? 0;
   for (let i = 0; i < todos.length; i++) {
     let todo = todos[i];
-    // console.log(todo);
     const todoCard = document.createElement("div");
     todoCard.className = `card-style p-4`;
     todoCard.innerHTML = `
@@ -23,30 +19,59 @@ const displayTodos = (todos) => {
         <p>${todo?.title}</p>
         <p>${todo?.completed ? "Done ✅" : "Incomplete ❌"}</p>
         `;
-    todoContainer.appendChild(todoCard);
+    todoContainer.prepend(todoCard);
   }
 };
 
+const loadTodos = async () => {
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+    const data = await res.json();
+    allTodo = data;
+    displayTodos(allTodo);
+  } catch (err) {
+    console.log(err);
+  }
+  // finally{
+  //     console.log('Data Loaded');
+  // }
+};
 
-const  loadTodos  = async() => {
-    try{
-        const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
-        const data = await res.json();
-        displayTodos(data);
-    }
-    catch(err){
-        console.log(err);
-    }
-    // finally{
-    //     console.log('Data Loaded');
-    // }
-}
+document.getElementById("submitBtn").addEventListener("click", async () => {
+  const todoFieldVal = document.getElementById("todo-field").value.trim();
+  if (!todoFieldVal || todoFieldVal.length < 4) return alert();
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      userId: 6,
+      title: todoFieldVal,
+      completed: true,
+    }),
+  });
 
+  const data = await res.json();
+  allTodo.push(data);
+  displayTodos(allTodo);
+});
 
+const updateTodo = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/200`, {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      title: "Kamal Hain",
+    }),
+  });
+  const data = await res.json();
+  console.log(data);
+};
 
-loadTodos()
-
-
+loadTodos();
 // const user = {
 //     name: 'Shafayet',
 //     age: 12,
@@ -58,7 +83,6 @@ loadTodos()
 //     const val = users[1];
 //     console.log(key, val);
 // }
-
 
 //  [undefined, 2, 3, 4] map
 // const result = [1,2, 3, 4].map(n => {
