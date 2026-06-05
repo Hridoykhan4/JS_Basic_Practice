3345c78899183d817786ec7cc84045574b6b9457
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-06-06 00:40:42 +0600

    add hoisting example in hoisting.js

---
+++ A Basic Intro/hoisting.js
@@ -0,0 +1,15 @@
+// console.log(a);
+// var a = 5
+
+// const one = function () {
+//     console.log('One');
+// }
+// one()
+
+console.log(b);
+const b = 10
+
+
+// function one () {
+//     console.log('One');
+// }
