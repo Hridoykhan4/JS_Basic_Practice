12eafa5ef41d67123a2b4667186f1346ab0b9904
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-06-06 17:09:04 +0600

    add initial switch statement example in switch.js and block scope example in scope.js

---
+++ A Basic Intro/scope.js
@@ -0,0 +1,17 @@
+// let abc; 
+// function scp () {
+//      abc = 10;
+// }
+// console.log(abc);
+
+// scp()
+// Undefined hobe for block scope
+
+// function scp () {
+//      for(var i = 0; i < 5; i++) {
+//           console.log(i);
+//      }
+//      console.log(i);
+// }
+
+// scp()
---
+++ A Basic Intro/switch.js
@@ -0,0 +1,33 @@
+// const number = 32;
+
+// switch (number) {
+//   case 1:
+//     console.log("One");
+//     break;
+//   case 2:
+//     console.log("two");
+//     break;
+
+//     default: console.log('Invalid');
+// }
+
+let day = 2;
+let dayName;
+
+switch (day) {
+  case 1:
+    dayName = "saturday";
+    break;
+
+  case 2:
+    dayName = "sunday";
+    break
+
+   case 3: 
+    dayName = 'monday' 
+
+  default:
+    dayName = 'Please enter between 1 - 7'
+}
+
+console.log(dayName);
